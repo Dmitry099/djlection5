ef26621e332a3f859a1ffbca899cfbd22e170049
Dmitry Lomaev <lomaevd@gmail.com>
2020-01-04 13:25:42 +0400

    PEP fixes

--- orm_query/articles/views.py
+++ orm_query/articles/views.py
@@ -7,7 +7,8 @@ from .models import Article
 def articles_list(request):
     template_name = 'articles/news.html'
 
-    articles = Article.objects.defer('published_at').select_related('author', 'genre').defer('author__phone').order_by('-published_at')
+    articles = (Article.objects.defer('published_at').select_related('author', 'genre').
+                defer('author__phone').order_by('-published_at'))
 
     context = {'object_list': articles}
 
